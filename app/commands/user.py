758abcd9be783c1390efac563eb8ee8517402cf9
Trung Nguyen <Trung_Nguyen-HANO@external.mckinsey.com>
2021-12-27 15:37:06 +0700

    Implement functions to borrow a book, return a book, list all borrowing book

--- app/commands/user.py
+++ app/commands/user.py
@@ -49,3 +49,29 @@ def delete(identity):
     """Delete a user by id"""
     service.delete(identity)
     click.echo("Deleted")
+
+
+@user.command()
+@click.option("-u", "--user-id", required=True, type=int)
+@click.option("-b", "--book-id", required=True, type=int)
+def borrow(user_id, book_id):
+    """User borrow a book from the library"""
+    service.borrow_book(user_id=user_id, book_id=book_id)
+    click.echo("Success")
+
+
+@user.command("return")
+@click.option("-u", "--user-id", required=True, type=int)
+@click.option("-b", "--book-id", required=True, type=int)
+def return_book(user_id, book_id):
+    """User return a book to the library"""
+    service.return_book(user_id=user_id, book_id=book_id)
+    click.echo("Success")
+
+
+@user.command()
+@click.option("-u", "--user-id", required=True, type=int)
+def borrowing_list(user_id):
+    """List the borrowing list of a user by user id"""
+    borrowing = service.borrowing_list(user_id=user_id)
+    click.echo(f"Borrowing list: {borrowing}")
